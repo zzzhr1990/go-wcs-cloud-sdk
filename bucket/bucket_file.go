72e64345e7a7faa7debf4572c193732372952894
zzzhr <zzzhr@hotmail.com>
2019-04-25 03:22:26 +0800

    Add copy

--- bucket/bucket_file.go
+++ bucket/bucket_file.go
@@ -2,46 +2,56 @@ package bucket
 
 import (
 	"errors"
+	"github.com/zzzhr1990/go-wcs-cloud-sdk/core"
 	"github.com/zzzhr1990/go-wcs-cloud-sdk/utility"
-
 )
 
 //Stat star
 // 获取文件信息（stat）
 // https://wcs.chinanetcenter.com/document/API/ResourceManage/stat
-func (manager *Manager) Stat(bucket string, key string) (*StatResult,  error) {
+func (manager *Manager) Stat(bucket string, key string) (*StatResult, error) {
 	if 0 == len(bucket) {
 		err := errors.New("bucket is empty")
-		return nil,err
+		return nil, err
 	}
 	if 0 == len(key) {
 		err := errors.New("key is empty")
-		return nil,err
+		return nil, err
 	}
 
 	url := manager.config.GetManageURLPrefix() + "/stat/" + utility.URLSafeEncodePair(bucket, key)
 	request, err := utility.CreateGetRequest(url)
 	if nil != err {
-		return nil,err
+		return nil, err
 	}
 	res := &StatResult{}
 	err = manager.httpManager.DoWithAuthRetry(request, manager.auth, res, 10)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	return res, nil
 }
 
-/*
-{
-    "result":  "true",
-    "code":  "200",
-    "name":  "<fileName string>",
-    "message":  "< message string>",
-    "fsize":     "<FileSize  int>",
-    "hash":     "<FileETag  string>",
-    "mimeType:  "<MimeType  string>",
-    "putTime":    "<PutTime   int64>"
-    "expirationDate":   "<ExpirationDate string>"
+//Copy (copy)
+// https://wcs.chinanetcenter.com/document/API/ResourceManage/copy
+func (manager *Manager) Copy(src string, dst string) (*core.CommonResponse, error) {
+	if 0 == len(src) {
+		return nil, errors.New("src is empty")
+	}
+	if 0 == len(dst) {
+		return nil, errors.New("dst is empty")
+	}
+
+	url := manager.config.GetManageURLPrefix() + "/copy/" + utility.URLSafeEncodeString(src) +
+		"/" + utility.URLSafeEncodeString(dst)
+	request, err := utility.CreatePostRequest(url)
+	if nil != err {
+		return nil, err
+	}
+	respEntity := &core.CommonResponse{}
+	err = manager.httpManager.DoWithAuthRetry(request, manager.auth, respEntity, 10)
+	if err != nil {
+		return nil, err
+	}
+	return respEntity, nil
 }
-*/
---
+++ core/common_response.go
@@ -0,0 +1,8 @@
+package core
+
+
+//CommonResponse response
+type CommonResponse struct {
+	Code           int    `json:"code,omitempty"`
+	Message        string `json:"message,omitempty"`
+}
