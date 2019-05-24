03456ef42575a43e0ebc2d89df269733de7b6aa2
zzzhr <zzzhr@hotmail.com>
2019-05-24 15:59:19 +0800

    dd map

---
+++ bucket/bucket_fops.go
@@ -0,0 +1,43 @@
+package bucket
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/zzzhr1990/go-wcs-cloud-sdk/core"
+	"github.com/zzzhr1990/go-wcs-cloud-sdk/utility"
+)
+
+// Fops dp fops
+func (manager *Manager) Fops(query string) (*core.CommonResponse, error) {
+	if 0 == len(query) {
+		return nil, errors.New("query is empty")
+	}
+
+	url := manager.config.GetManageURLPrefix() + "/fops"
+	request, err := utility.CreatePostRequest(url)
+	if nil != err {
+		return nil, err
+	}
+	request.AddStringBody(query)
+	respEntity := &core.CommonResponse{}
+	err = manager.httpManager.DoWithAuthRetry(request, manager.auth, respEntity, 10)
+	if err != nil {
+		return nil, err
+	}
+	return respEntity, nil
+}
+
+// FopsMap dp fops
+func (manager *Manager) FopsMap(query map[string]string) (*core.CommonResponse, error) {
+	if query == nil || 0 == len(query) {
+		return nil, errors.New("query is empty")
+	}
+
+	ss := []string{}
+	for key, value := range query {
+		ss = append(ss, key+"="+value)
+	}
+
+	return manager.Fops(strings.Join(ss, "&"))
+}
